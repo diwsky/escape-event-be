4b478b65fb2265e8d19328031aaa64b7a9e92fe0
Diwangkoro <diwangkoro@Nabhilas-MacBook-Air-10.local>
2025-03-09 10:04:03 +0700

    remove aws

--- config/plugins.js
+++ config/plugins.js
@@ -10,15 +10,15 @@ module.exports = ({ env }) => ({
         publicFiles: env("GCS_PUBLIC_FILES"),
         uniform: env("GCS_UNIFORM"),
       },
-      provider: "aws-s3",
-      providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET_NAME"),
-        },
-      },
+      // provider: "aws-s3",
+      // providerOptions: {
+      //   accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      //   secretAccessKey: env("AWS_ACCESS_SECRET"),
+      //   region: env("AWS_REGION"),
+      //   params: {
+      //     Bucket: env("AWS_BUCKET_NAME"),
+      //   },
+      // },
       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
         upload: {
